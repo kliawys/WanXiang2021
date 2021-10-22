4414f706c3c00dd697996205a11720238de6c6eb
Danny Liu <327651321@qq.com>
2021-10-22 15:48:21 +0800

    Add files via upload

---
+++ build/precache-manifest.4ee25321e329a8b41df75786bacec705.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c0f857a88c2fb8dc23e0d84fa2ce28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4bbe07408cef4f1dd15",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "83ccaae0b0807c668a5f",
+    "url": "/static/js/2.24535568.chunk.js"
+  },
+  {
+    "revision": "e4bbe07408cef4f1dd15",
+    "url": "/static/js/main.9b0e8464.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,177 @@
+import React, { Component } from "react";
+import VotingContract from "./contracts/SimpleStorage.json";
+import getWeb3 from "./getWeb3";
+
+import "./App.css";
+// const contract_add='0xc6185b4476626b98cbaecad4a87872e0be617063';
+
+
+class App extends Component {
+  state = { storageValue: 0, web3: null, accounts: null, contract: null, get_set_piaoshu: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = VotingContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        VotingContract.abi,
+        "0x8d061bd124ff77ed1952fc3b7bd5dc45bdb5807a",
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  runExample = async () => {
+    const { accounts, contract } = this.state;
+
+    // Stores a given value, 5 by default.
+     await contract.methods.set(5).send({ from: accounts[0] });
+
+    // Get the value from the contract to prove it worked.
+
+    this.state.contract.methods.chakan('xiaobai').call().then(
+      t => this.setState({ storageValue: t })
+    );
+    
+
+
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        {/* <h1>Good to Go!</h1>
+        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 40</strong> of App.js.
+        </p>
+        <div>The stored value is: {this.state.storageValue}</div> */}
+        小白的票数是：{this.state.storageValue}<br></br><br></br>
+        <button onClick={()=>{
+          this.state.contract.methods.toupiao('xiaobai').send({from:this.state.accounts[0]})
+        }
+        }>投票给小白</button><br></br><br></br>
+
+        <button onClick={()=>{
+          this.state.contract.methods.chakan('xiaobai').call().then(
+            t => this.setState({ storageValue: t })
+          );
+        }
+        }>查看小白的票数</button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+/* 
+  
+import React, { Component } from "react";
+import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import getWeb3 from "./getWeb3";
+
+import "./App.css";
+
+class App extends Component {
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        "0xAEDFCAAf045a76BdbC5d4F52747E6f1fA202D184",
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  runExample = async () => {
+    const { accounts, contract } = this.state;
+
+    // Stores a given value, 5 by default.
+    await contract.methods.set(5).send({ from: accounts[0] });
+
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.get().call();
+
+    // Update state with the result.
+    this.setState({ storageValue: response });
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <h1>Good to Go!</h1>
+        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 42</strong> of App.js.
+        </p>
+        <div>The stored value is: {this.state.storageValue}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+*/
